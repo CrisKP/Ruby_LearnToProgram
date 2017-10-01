0037a32afb1051e940c997322011218e258915f6
cris <crispope@gmail.com>
2017-09-30 20:27:55 -0700

    formatting a table

---
+++ tableOfContents.rb
@@ -0,0 +1,6 @@
+#Format a Table of Contents with centered header and 2 columns
+lineWidth = 50
+puts 'Table of Contents'.center(lineWidth)
+puts 'Chapter 1: Numbers'.ljust(lineWidth/2) + 'page 1'.rjust(lineWidth/2)
+puts 'Chapter 2: Letters'.ljust(lineWidth/2) + 'page 72'.rjust(lineWidth/2)
+puts 'Chapter 3: Variables'.ljust(lineWidth/2) + 'page 118'.rjust(lineWidth/2)
